9940a615078a881e13f93f682e6e1d4c3f0cc2f5
saisowjanyavemulapudi <saisowjanya103@gmail.com>
2024-06-20 17:08:06 +0530

    sowjanya

---
+++ src/com/demo/DatatypesDemo.java
@@ -0,0 +1,51 @@
+package com.demo;
+
+public class DatatypesDemo {
+	public static void main(String[] args) {
+		
+		System.out.println("Escape Sequence :-");
+		int age = 22;
+		
+		short date = 20;
+		System.out.println(age+"\n\n\t"+date);
+		System.out.println("\nAptitude sample Que :-");
+		int i = 100;
+		
+		System.out.println(i);
+		System.out.println(i+100);
+		System.out.println("i"+100);
+		System.out.println("100"+i);
+		
+		byte idNo = 20;
+		System.out.println("Employee id number :"+idNo);
+		
+		short pinNo = 4540;
+		System.out.println("Employee pin number :"+pinNo);
+		
+		int regNo = 4512453;
+		System.out.println("Employee register number :"+regNo);
+		
+		long phNo = 7845985202l;
+		System.out.println("Employee phone number :"+phNo);
+		
+		float ave = 20.5f;
+		System.out.println("Employee Average :"+ave);
+		
+		double lattitude = 25255455.22346;
+		System.out.println("Employee Map lattitude :"+lattitude);
+		
+		char gender = 'm';
+		System.out.println("Employee gender :"+gender);
+		
+		boolean result = true;
+		System.out.println("All above informations are"+result+".");
+		 
+		String email = "Abc123@gmail.com";
+		System.out.println("Employee E Mail address :"+email);
+		
+		
+		
+		
+	}
+
+}
